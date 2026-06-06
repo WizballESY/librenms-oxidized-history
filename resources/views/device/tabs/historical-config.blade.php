929b04ca57b851bd0cee07c6561a89b35c555b05
WizballESY <tsbjerk@gmail.com>
2026-06-06 17:36:16 +0200

    Render historical config tab inside device layout

--- resources/views/device/tabs/historical-config.blade.php
+++ resources/views/device/tabs/historical-config.blade.php
@@ -1,68 +1,74 @@
-<x-panel title="Historical Config">
-    <div class="row">
-        <div class="col-md-12">
-            <p class="text-muted">
-                Stored Oxidized configuration history from the configured history API.
-            </p>
+@extends('layouts.librenmsv1')
 
-            <table class="table table-condensed">
-                <tbody>
-                    <tr>
-                        <th style="width: 180px;">Device</th>
-                        <td>{{ $device->hostname }}</td>
-                    </tr>
-                    <tr>
-                        <th>Resolved Oxidized node</th>
-                        <td>
-                            @if(($data['resolved']['node_full'] ?? null))
-                                <code>{{ $data['resolved']['node_full'] }}</code>
-                            @else
-                                <span class="text-danger">Not resolved</span>
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>History API</th>
-                        <td><code>{{ $data['api_url'] ?? '' }}</code></td>
-                    </tr>
-                </tbody>
-            </table>
+@section('content')
+<x-device.page :device="$device">
+    <x-panel title="Historical Config">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-muted">
+                    Stored Oxidized configuration history from the configured history API.
+                </p>
 
-            @if(!($data['history']['ok'] ?? false))
-                <div class="alert alert-warning">
-                    <strong>No historical config available.</strong>
-                    <br>
-                    {{ $data['history']['error'] ?? 'Unknown error' }}
-                    @if(isset($data['history']['status']) && $data['history']['status'])
-                        <br>
-                        HTTP status: {{ $data['history']['status'] }}
-                    @endif
-                </div>
-            @else
-                <div class="alert alert-success">
-                    <strong>Historical config found.</strong>
-                    {{ count($data['history']['versions'] ?? []) }} stored versions returned by the history API.
-                </div>
-
-                <table class="table table-condensed table-striped">
-                    <thead>
-                        <tr>
-                            <th style="width: 220px;">Time</th>
-                            <th style="width: 330px;">Commit</th>
-                            <th>Message</th>
-                        </tr>
-                    </thead>
+                <table class="table table-condensed">
                     <tbody>
-                        @foreach(($data['history']['versions'] ?? []) as $version)
-                            <tr>
-                                <td>{{ $version['time'] ?? $version['date'] ?? '' }}</td>
-                                <td><code>{{ $version['oid'] ?? '' }}</code></td>
-                                <td>{{ $version['message'] ?? '' }}</td>
-                            </tr>
-                        @endforeach
+                        <tr>
+                            <th style="width: 180px;">Device</th>
+                            <td>{{ $device->hostname }}</td>
+                        </tr>
+                        <tr>
+                            <th>Resolved Oxidized node</th>
+                            <td>
+                                @if(($data['resolved']['node_full'] ?? null))
+                                    <code>{{ $data['resolved']['node_full'] }}</code>
+                                @else
+                                    <span class="text-danger">Not resolved</span>
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>History API</th>
+                            <td><code>{{ $data['api_url'] ?? '' }}</code></td>
+                        </tr>
                     </tbody>
                 </table>
-            @endif
+
+                @if(!($data['history']['ok'] ?? false))
+                    <div class="alert alert-warning">
+                        <strong>No historical config available.</strong>
+                        <br>
+                        {{ $data['history']['error'] ?? 'Unknown error' }}
+                        @if(isset($data['history']['status']) && $data['history']['status'])
+                            <br>
+                            HTTP status: {{ $data['history']['status'] }}
+                        @endif
+                    </div>
+                @else
+                    <div class="alert alert-success">
+                        <strong>Historical config found.</strong>
+                        {{ count($data['history']['versions'] ?? []) }} stored versions returned by the history API.
+                    </div>
+
+                    <table class="table table-condensed table-striped">
+                        <thead>
+                            <tr>
+                                <th style="width: 220px;">Time</th>
+                                <th style="width: 330px;">Commit</th>
+                                <th>Message</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach(($data['history']['versions'] ?? []) as $version)
+                                <tr>
+                                    <td>{{ $version['time'] ?? $version['date'] ?? '' }}</td>
+                                    <td><code>{{ $version['oid'] ?? '' }}</code></td>
+                                    <td>{{ $version['message'] ?? '' }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @endif
+            </div>
         </div>
-    </div>
-</x-panel>
+    </x-panel>
+</x-device.page>
+@endsection
